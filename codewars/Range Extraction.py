8ef3eabd42bfc5eb10655b4a43a9b81e67d78731
Sami Shakir <sami.shakir@hostopia.com.au>
2020-03-10 13:04:26 +1100

    auto commit from $(hostname)

--- codewars/Range Extraction.py
+++ codewars/Range Extraction.py
@@ -1,38 +1,26 @@
 def solution(args):
     total_items = range(len(args))
     count = 0
-
+    new_arr = []
     for i in total_items:
         curr_num = args[i]
-        next_num = args[i + 1]
+        try:
+            next_num = args[i + 1]
+        except IndexError:
+            next_num = None
         if curr_num + 1 == next_num:
             count+=1
         else:
-            if count >= 3:
-                print(f"{args[i - count]}-{args[i]}")
-            elif count < 3:
-                print(f"{args[i:i + count]}")
+            if count >= 2:
+                new_arr.append(f"{args[i - count]}-{args[i]}")
+            elif count == 1:
+                new_arr.append(f"{args[i-1]}")
+                new_arr.append(f"{args[i]}")
             else:
-                print(f"single item {args[i]}")
+                new_arr.append(f"{args[i]}")
             count = 0
 
-'''
-is this num == next?
-count++
-
-is next == after?
-count++
-etc.
-
-until not - break and then:
-take index of 'this num' to index of 'this num + count' and stringify '{this num}-{this num + count}'
-continue on from 'this num + count + 1'
-
-
-
-
-'''
-
+    return ",".join(new_arr)
 
 
 solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20])
