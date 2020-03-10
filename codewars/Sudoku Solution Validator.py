a6fec4c8a603ffa7710b774d73533080211f39fc
Sami Shakir <sami.shakir@hostopia.com.au>
2020-03-10 16:19:37 +1100

    auto commit from $(hostname)

---
+++ codewars/Sudoku Solution Validator.py
@@ -0,0 +1,62 @@
+# https://www.codewars.com/kata/529bf0e9bdf7657179000008/train/python
+def valid_solution(board):
+    ans = True
+    length = range(len(board))
+    print(length)
+    arr = []
+    for line in board:
+        for num in line:
+            for item in length:
+                if num not in arr:
+                    arr.append(num)
+                elif num not in board[length]
+
+            else:
+                ans = False
+                break
+        
+
+        #     else:
+        #         for num in length:
+        #             if num in board[num]:
+        #                 ans = False
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+valid_solution([
+  [5, 3, 4, 6, 7, 8, 9, 1, 2],
+  [6, 7, 2, 1, 9, 5, 3, 4, 8],
+  [1, 9, 8, 3, 4, 2, 5, 6, 7],
+  [8, 5, 9, 7, 6, 1, 4, 2, 3],
+  [4, 2, 6, 8, 5, 3, 7, 9, 1],
+  [7, 1, 3, 9, 2, 4, 8, 5, 6],
+  [9, 6, 1, 5, 3, 7, 2, 8, 4],
+  [2, 8, 7, 4, 1, 9, 6, 3, 5],
+  [3, 4, 5, 2, 8, 6, 1, 7, 9]
+]) # => true
+
+valid_solution([
+  [5, 3, 4, 6, 7, 8, 9, 1, 2], 
+  [6, 7, 2, 1, 9, 0, 3, 4, 8],
+  [1, 0, 0, 3, 4, 2, 5, 6, 0],
+  [8, 5, 9, 7, 6, 1, 0, 2, 0],
+  [4, 2, 6, 8, 5, 3, 7, 9, 1],
+  [7, 1, 3, 9, 2, 4, 8, 5, 6],
+  [9, 0, 1, 5, 3, 7, 2, 1, 4],
+  [2, 8, 7, 4, 1, 9, 6, 3, 5],
+  [3, 0, 0, 4, 8, 1, 1, 7, 9]
+]) # => false
